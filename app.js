c0e091b88f7717e5c3478b61f047c84e5c82a5a4
Pepe Valdivia <jvaldivia@softweb.pe>
2019-08-01 09:05:32 -0500

    usando middlewares de preResponse y logs

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const Koa = require('koa');
 const static = require('koa-static');
 // export configs
 // const sockets = require('./config/sockets');
-// const middlewares = require('./config/middlewares');
 // const constants = require('./config/constants');
+const middlewares = require('./config/middlewares');
 const bootstrap = require('./config/bootstrap');
 // new app
 const app = new Koa();
 // static files
 app.use(static(__dirname + '/public'));
+// middlewares
+app.use(middlewares.preResponse());
+app.use(middlewares.showLogs());
 // forward routes
 bootstrap(app);
 // port
