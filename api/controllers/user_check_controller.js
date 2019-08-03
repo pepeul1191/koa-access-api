0cd3dfa7b8b7ad7137590915bf6c33c0b43d5386
Pepe Valdivia <jvaldivia@softweb.pe>
2019-08-03 12:53:07 -0500

    Operaciones user/check en su propio controlador

--- api/controllers/user_check_controller.js
+++ api/controllers/user_check_controller.js
@@ -8,69 +8,30 @@ const db = require('../../config/database');
 
 let router = new Router();
 
-router.post('/user/create', [
+router.post('/user/check', [
   //middlewares.sessionRequiredFalse,
   async (ctx, next) => {
-    var resp = {};
+    var resp = '';
     var status = 200;
-    // get user with user and email
-    var temp = await User.findOne({
-      $or: [
-        {
-          user: ctx.request.body.user
-        },
-        {
-          email: ctx.request.body.email
-        },
-      ]
-    }).exec();
-    if(temp != null){
-      // error, neither user nor email are unique
+    // get count users with user and pass
+    var user = await userCheck(
+      ctx.request.body.user,
+      ctx.request.body.pass
+    );
+    if(user.length == 0){
       status = 409;
-      resp.action_executed = 'none';
-      resp.data = 'Usuario y/o correo repetidos';
     }else{
-      // create user
-      var user = new User({
-        user: ctx.request.body.user,
-        pass: ctx.request.body.pass,
-        email: ctx.request.body.email,
-        profile_picture: 'default_user.png',
-        state_id: getStatusId('activation_pending'),
-        activation_key: random(24),
-        reset_key: random(24),
-        systems: [{
-          system_id: db.mongoose.Types.ObjectId(ctx.request.body.system_id),
-          permissions_id: [],
-        }],
-      });
-      var new_user = await user.save();
-      resp.action_executed = 'create';
-      resp._id = new_user._id;
-      resp.activation_key = new_user.activation_key;
+      // get status
+      resp = user[0].state;
     }
     // response
     ctx.set('Content-Type', 'text/html; charset=utf-8');
     ctx.status = status;
-    ctx.body = JSON.stringify(resp);
+    ctx.body = resp;
   }
 ]);
 
-router.post('/user/delete', [
-  //middlewares.sessionRequiredFalse,
-  async (ctx, next) => {
-    var resp = {};
-    var status = 200;
-    // get user with user and email
-    await User.findByIdAndDelete(ctx.request.body._id);
-    // response
-    ctx.set('Content-Type', 'text/html; charset=utf-8');
-    ctx.status = status;
-    ctx.body = JSON.stringify(resp);
-  }
-]);
-
-router.post('/user/reset', [
+router.post('/user/check/activate', [
   //middlewares.sessionRequiredFalse,
   async (ctx, next) => {
     var resp = {};
@@ -79,7 +40,53 @@ router.post('/user/reset', [
     var temp = await User.findOne({
       $and: [
         {
-          email: ctx.request.body.email
+          _id: ctx.request.body._id
+        },
+        {
+          activation_key: ctx.request.body.activation_key
+        },
+      ]
+    }).exec();
+    if(temp == null){
+      // error, neither activation_key nor _id not exist
+      status = 409;
+      resp.action_executed = 'none';
+      resp.data = 'Código de activación errado';
+    }else{
+      // update user state and change reset_key
+      await User.findOneAndUpdate(
+        {
+          _id: ctx.request.body._id
+        },
+        {
+          $set: {
+            state_id: getStatusId('active'),
+            activation_key: random(24),
+          }
+        }
+      );
+      resp.action_executed = 'activated';
+    }
+    // response
+    ctx.set('Content-Type', 'text/html; charset=utf-8');
+    ctx.status = status;
+    ctx.body = JSON.stringify(resp);
+  }
+]);
+
+router.post('/user/check/reset', [
+  //middlewares.sessionRequiredFalse,
+  async (ctx, next) => {
+    var resp = {};
+    var status = 200;
+    // get user with user and email
+    var temp = await User.findOne({
+      $and: [
+        {
+          _id: ctx.request.body._id
+        },
+        {
+          reset_key: ctx.request.body.reset_key
         },
       ]
     }).exec();
@@ -87,22 +94,21 @@ router.post('/user/reset', [
       // error, neither reset_key nor _id not exist
       status = 409;
       resp.action_executed = 'none';
-      resp.data = 'Correo no registrado';
+      resp.data = 'Código de cambio de contraseña errado';
     }else{
       // update user state and change reset_key
-      var temp = await User.findOneAndUpdate(
+      await User.findOneAndUpdate(
         {
-          email: ctx.request.body.email
+          _id: ctx.request.body._id
         },
         {
           $set: {
+            pass: ctx.request.body.pass,
             reset_key: random(24),
           }
         }
       );
       resp.action_executed = 'updated';
-      resp.reset_key = temp.reset_key;
-      resp._id = temp._id;
     }
     // response
     ctx.set('Content-Type', 'text/html; charset=utf-8');
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,7 +1,9 @@
 const homeRouter = require('../api/controllers/home_controller');
 const userRouter = require('../api/controllers/user_controller');
+const userCheckRouter = require('../api/controllers/user_check_controller');
 
 module.exports = function(app){
   app.use(homeRouter.routes);
   app.use(userRouter.routes);
+  app.use(userCheckRouter.routes);
 };
